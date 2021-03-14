2b6cb706be0cd785a2f3d69163f94d19cb02f7ac
Will Cohen <will.r.cohen@gmail.com>
2021-03-14 15:56:51 -0400

    feat: Add action constants

---
+++ src/redux/actionConstants.js
@@ -0,0 +1,9 @@
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+export const INVALID_LOGIN = "INVALID_LOGIN";
+export const LOGIN_NETWORK_ERROR = "LOGIN_NETWORK_ERROR";
+export const LOGOUT_SUCCESS = "LOGOUT_SUCCESS";
+export const READ_JOBS = "READ_JOBS";
+export const SAVE_JOB = "SAVE_JOB";
+export const READ_SAVED_JOBS = "READ_SAVED_JOBS";
+export const SORT_JOBS = "SORT_JOBS";
+export const UPDATE_ONBOARDING = "UPDATE_ONBOARDING";
