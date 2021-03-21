3f8c600d09241cc9a40c6de016be33cbc694a2a2
Will Cohen <will.r.cohen@gmail.com>
2021-03-21 11:20:02 -0400

    feat: Style sorting button

--- src/components/JobButtons.js
+++ src/components/JobButtons.js
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSortAmountDown } from "@fortawesome/free-solid-svg-icons";
 import { LANGUAGES } from "../data/data";
 import { sortBySaves, readJobs } from "../redux/actions";
 
@@ -21,22 +23,24 @@ const JobButtons = () => {
             <div className="btn-group" role="group">
                 {
                     Object.keys(LANGUAGES).map((lang, index) =>
-                        <>
-                            <label key={index} className="btn btn-outline-primary" autocomplete="off"
-                                htmlFor={`languageRadio${index-1}`}
-                            >
-                                {LANGUAGES[lang]}                                
-                            </label>
+                        <div key={index}>
                             <input type="radio" name="languageSelection" 
                                 className="btn-check" id={`languageRadio${index}`} 
                                 onChange={() => dispatch(readJobs(LANGUAGES[lang]))}
                             />
-                        </>
+                            <label className="btn btn-outline-primary" autocomplete="off"
+                                htmlFor={`languageRadio${index}`}
+                            >
+                                {LANGUAGES[lang]}                                
+                            </label>
+                        </div>
                     )
                 }
             </div>
             {/* <button onClick={() => dispatch(readJobs(null))}>My Saved Jobs</button> */}
-            <button onClick={() => dispatch(sortBySaves())}>Sort By Popularity</button>
+            <button className="btn btn-primary" onClick={() => dispatch(sortBySaves())}>
+                Sort By Popularity <FontAwesomeIcon icon={faSortAmountDown} aria-hidden="true" />
+            </button>
         </div>
     );
 };
