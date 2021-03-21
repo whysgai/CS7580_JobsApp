6daeb83c55efdf74291be4799b5343158ed48619
Will Cohen <will.r.cohen@gmail.com>
2021-03-21 11:03:29 -0400

    style: Move Job elements around

--- src/components/Job.js
+++ src/components/Job.js
@@ -10,25 +10,25 @@ const Job = (props) => {
     return (
         <div className="accordion-item">
             <div className="accordian-header" id={`jobHeading${props.job.id}`}>
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target={`#jobCollapse${props.job.id}`} aria-expanded="false" aria-controls={`jobCollapse${props.job.id}`}>
+                <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target={`#jobCollapse${props.job.id}`} aria-expanded="false" aria-controls={`jobCollapse${props.job.id}`}>
                 <span>{props.job.title}</span>
                 <span>{props.job.company}</span>
-                {
-                    user.saved.includes(props.job.id) ?
-                        <button
-                            onClick={() => dispatch(toggleSaved(true, props.job.id))}
-                        >Saved</button>
-                        :
-                        <button 
-                            onClick={() => dispatch(toggleSaved(false, props.job.id))}
-                        >Save</button>
-                }
                 </button>
             </div>
-            <div id={`jobCollapse${props.job.id}`} class="accordion-collapse collapse" aria-labelledby={`jobHeading${props.job.id}`} data-bs-parent="#jobsAccordion">
+            <div id={`jobCollapse${props.job.id}`} className="accordion-collapse collapse" aria-labelledby={`jobHeading${props.job.id}`} data-bs-parent="#jobsAccordion">
                 <div className="accordion-body">
                     <p>Job ID: {props.job.id}</p>
                     <p>{props.job.description}</p> 
+                    {
+                        user.saved.includes(props.job.id) ?
+                            <button
+                                onClick={() => dispatch(toggleSaved(true, props.job.id))}
+                            >Saved</button>
+                            :
+                            <button 
+                                onClick={() => dispatch(toggleSaved(false, props.job.id))}
+                            >Save</button>
+                    }
                 </div>
             </div>                   
         </div>
