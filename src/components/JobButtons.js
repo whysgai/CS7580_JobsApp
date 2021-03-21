cdc8c53c05016aa8157b06225cea86b3cbf5cb11
Will Cohen <will.r.cohen@gmail.com>
2021-03-21 11:05:26 -0400

    refactor: Convert language selection buttons to radio

--- src/components/JobButtons.js
+++ src/components/JobButtons.js
@@ -1,20 +1,40 @@
+import { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { LANGUAGES } from "../data/data";
 import { sortBySaves, readJobs } from "../redux/actions";
 
 const JobButtons = () => {
 
+    // If searching for multiple languages, store here?
+    const [selectedLanguages, setSelectedLanguages] = useState([]);
+
     const dispatch = useDispatch();
+
+    useEffect(() => {
+        // Refactor to allow searching for multiple jobs at once?
+    }, [selectedLanguages]);
 
     return (
         <div>
             {/* <button onClick={() => dispatch(readJobs(LANGUAGES.ALL, null))}>All</button> */}
             {/* {Object.keys(LANGUAGES).forEach((lang, index) => {console.log(lang)})} */}
-            {
-                Object.keys(LANGUAGES).map((lang, index) =>
-                    <button key={index} onClick={() => dispatch(readJobs(LANGUAGES[lang]))}>{LANGUAGES[lang]}</button>
-                )
-            }
+            <div className="btn-group" role="group">
+                {
+                    Object.keys(LANGUAGES).map((lang, index) =>
+                        <>
+                            <label key={index} className="btn btn-outline-primary" autocomplete="off"
+                                htmlFor={`languageRadio${index-1}`}
+                            >
+                                {LANGUAGES[lang]}                                
+                            </label>
+                            <input type="radio" name="languageSelection" 
+                                className="btn-check" id={`languageRadio${index}`} 
+                                onChange={() => dispatch(readJobs(LANGUAGES[lang]))}
+                            />
+                        </>
+                    )
+                }
+            </div>
             {/* <button onClick={() => dispatch(readJobs(null))}>My Saved Jobs</button> */}
             <button onClick={() => dispatch(sortBySaves())}>Sort By Popularity</button>
         </div>
